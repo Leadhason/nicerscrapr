2bacfff3f0bd632eec1d95d113968133abd70e43
Leadhason <leadhason111@gmail.com>
2024-12-25 00:15:45 +0000

    header and hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+interface UrlInputProps {
+  onScrape: (url: string) => Promise<void>
+}
+  
+
+export default function Home({ onScrape }: UrlInputProps) {
+  const [url, setUrl] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    onScrape(url)
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col h-screen container mx-auto p-4">
+      <header className="flex w-full justify-between">
+        <h1 className="text-4xl font-bold text-center text-gray-900">
+            SCRAPR
+        </h1>
+        <button className="flex border w-[100px] h-[40px] items-center justify-center border-black p-3 text-white bg-black hover:text-black hover:bg-transparent rounded-full">GitHub</button>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-col items-center justify-center h-full space-y-14">
+        <div className="-mt-48 space-y-3">
+          <h2 className="text-5xl font-bold text-center text-gray-900">
+            A Webscraper with <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">Superpowers</span>
+          </h2>
+          <p className="text-center text-lg font-extralight text-gray-400">
+            Simplify data collection with Scrapr, an intelligent web scraper <br/> that empowers you to extract and analyze website data effortlessly with AI precision.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <form onSubmit={handleSubmit} className="flex items-center justify-center h-[43px] w-[570px] bg-white rounded-md border border-gray-300 rounded-md shadow-md p-3 focus:border-black focus:ring-black focus:ring-1">
+          <Image src="/link.svg" width={25} height={25} alt="link" />
+          <input 
+            type="text" 
+            className="w-[550px] h-[35px] bg-tranparent p-1 outline-none" 
+            placeholder="Enter a URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            typeof="url"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button type="submit" className="bg-transparent border-none active:scale-90">
+            <Image src="/send.svg" width={30} height={30} alt="search" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
